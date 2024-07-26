1eaba2fbfb9117925832416fd7b22b1ed7b0a9e9
Sai Aparanji Nemmani <nsaiaparanji@gmail.com>
2024-07-26 17:33:53 -0400

    Header, intro, about and responsiveness

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ const switchElement = document.querySelector('.switch')
 switchElement.addEventListener('click', () => {
   document.body.classList.toggle('dark')
 })
+
+
+  const button = document.getElementById('hamButton');
+  const icon = button.querySelector('i');
+
+  button.addEventListener('click', () => {
+    if (icon.classList.contains('fa-bars')) {
+      icon.classList.replace('fa-bars', 'fa-arrow-left');
+    } else {
+      icon.classList.replace('fa-arrow-left', 'fa-bars');
+    }
+  });
