06bd016d1f26c513feb5016298dab3de59bf0b4a
Sai Aparanji Nemmani <nsaiaparanji@gmail.com>
2024-09-21 05:00:09 -0400

    updates

--- scripts.js
+++ scripts.js
@@ -16,16 +16,3 @@ button.addEventListener('click', () => {
   }
 });
 
-function sendEmail(){
-  Email.send({
-Host : "smtp.gmail.com",
-Username : "nsaiaparanji@gmail.com",
-Password : "password",
-To : 'them@website.com',
-From : "you@isp.com",
-Subject : "This is the subject",
-Body : "And this is the body"
-}).then(
-message => alert(message)
-);
-}
