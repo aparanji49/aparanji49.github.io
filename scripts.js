11ed028ec61bfc34c9f7df8997e4b7a47fe4ea43
Sai Aparanji Nemmani <nsaiaparanji@gmail.com>
2024-09-21 04:54:58 -0400

    updates-skills,contact

--- scripts.js
+++ scripts.js
@@ -15,3 +15,17 @@ button.addEventListener('click', () => {
     icon.classList.replace('fa-arrow-left', 'fa-bars');
   }
 });
+
+function sendEmail(){
+  Email.send({
+Host : "smtp.gmail.com",
+Username : "nsaiaparanji@gmail.com",
+Password : "password",
+To : 'them@website.com',
+From : "you@isp.com",
+Subject : "This is the subject",
+Body : "And this is the body"
+}).then(
+message => alert(message)
+);
+}
