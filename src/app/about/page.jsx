2c28bde407133f80f32fbf2a3a1f869413ac2fcc
Sai Aparanji Nemmani <nsaiaparanji@gmail.com>
2025-06-25 01:15:36 -0400

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,12 @@
 // src/app/about/page.jsx
 
+
 import About from "@/components/About";
 
 const AboutPage = () => {
   return (
     <>
+      {/* Designed & Developed by Sai Aparanji Nemmani – © 2025 */}
       <About />
     </>
   );
